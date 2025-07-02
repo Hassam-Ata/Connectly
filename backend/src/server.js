5e422c371bb4e4e154908815985fefa3ab68e472
Hassam-Ata <hassamata86@gmail.com>
2025-07-02 21:49:17 +0500

    completed backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+import chatRoutes from "./routes/chat.route.js";
 import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
 
@@ -7,7 +9,10 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/chat", chatRoutes);
 
 app.listen(PORT, () => {
   connectDB();
