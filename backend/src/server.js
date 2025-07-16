1a1128e4b0827ecd3ba86278f7c992424554dcf1
Hassam-Ata <hassamata86@gmail.com>
2025-07-16 23:42:04 +0500

    feat: frontend and backend URL can now be read from their respective .env files

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,14 +3,15 @@ import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import chatRoutes from "./routes/chat.route.js";
 import cookieParser from "cookie-parser";
-import cosrs from "cors";
+import cors from "cors";
 import { connectDB } from "./lib/db.js";
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+const NODE_ENV = process.env.NODE_ENV;
 app.use(
-  cosrs({
-    origin: "http://localhost:5173",
+  cors({
+    origin: process.env.FRONT_END_URL,
     credentials: true,
   })
 );
@@ -21,7 +22,20 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
 
-app.listen(PORT, () => {
-  connectDB();
-  console.log(`Server running on PORT ${PORT}`);
-});
+const startServer = async () => {
+  try {
+    await connectDB();
+
+    // listen for local development
+    if (NODE_ENV !== "production") {
+      app.listen(PORT, () =>
+        console.log("Server is up and running on PORT:", PORT)
+      );
+    }
+  } catch (error) {
+    console.error("Failed to start server:", error.message);
+    process.exit(1);
+  }
+};
+
+startServer();
